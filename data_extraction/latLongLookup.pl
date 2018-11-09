54e2f50706789c9ac95e0bcd567f67a6b0f3b9f0
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2018-11-08 17:16:00 -0700

    Add cleaned data with latitude and longitude entries; also add lookupTable data used to find the latitude and longitude, perl script used for lookup, and python script used for conversion to CSV

--- data_extraction/latLongLookup.pl
+++ data_extraction/latLongLookup.pl
@@ -1,6 +1,63 @@
 use strict;
 use warnings;
 
-use Parse::CSV;
+# read in lookup table
+my $lookupTableHandle;
+my %lookupTable;
 
+open($lookupTableHandle, "<", "lookupTable.csv") or die("Cannot open lookup table!");
+foreach my $lookupLine (<$lookupTableHandle>) {
+    chomp($lookupLine);
+    $lookupLine =~ /"(.*)","(.*)","(.*)","(.*)"/;
+    my $city = $1;
+    my $state = $2;
+    my $lat = $3;
+    my $lng = $4;
+    $lookupTable{"${city}_${state}"} = "${lat}_${lng}";
+}
+close($lookupTableHandle);
+#print("Test lookup:  Mukilteo, WA = $lookupTable{'Mukilteo_WA'}\n"); 
 
+# open the output file
+my $outputHandle;
+open($outputHandle, ">", "data.tsv") or die("Cannot open output file!");
+
+# open the notFound file
+my $notFoundHandle;
+open($notFoundHandle, ">", "notFound.tsv") or die("Cannot open notFound file!");
+
+# open the dataDump file
+my $inputHandle;
+open($inputHandle, "<", "dataDump.tsv") or die("Cannot open input file!");
+foreach my $line (<$inputHandle>) {
+    chomp($line);
+    # Sighting_Occurred|Sighting_Reported|Sighting_Location|UFO_Shape|Sighting_Duration|Sighting_Narrative
+    $line =~ /(.*)\|(.*)\|(.*)\|(.*)\|(.*)\|(.*)/;
+    my $sightingOccurred = $1;
+    my $sightingReported = $2;
+    my $sightingLocation = $3;
+    my $ufoShape = $4;
+    my $sightingDuration = $5;
+    my $sightingNarrative = $6;
+    #print("sightingOccurred=$sightingOccurred, sightingReported=$sightingReported, sightingLocation=$sightingLocation, ufoShape=$ufoShape, sightingDuration=$sightingDuration, sightingNarrative=$sightingNarrative\n");
+    if ($sightingLocation) {
+        $sightingLocation =~ /(.*), (\w+)/;
+        my $locationCity = $1;
+        my $locationState = $2;
+        #print("locationCity=$locationCity, locationState=$locationState\n");
+        my $latLong = $lookupTable{"${locationCity}_${locationState}"};
+        if ($latLong) {
+           $latLong =~ /(.*)_(.*)/; 
+           my $lat = $1;
+           my $lng = $2;
+           #print("lat=$lat, lng=$lng\n");
+           print $outputHandle  "$sightingOccurred|$sightingReported|$sightingLocation|$lat|$lng|$ufoShape|$sightingDuration|$sightingNarrative\n";
+        } else {
+           print $notFoundHandle "$sightingOccurred|$sightingReported|$sightingLocation|$ufoShape|$sightingDuration|$sightingNarrative\n";
+        }
+    }
+}
+
+close($inputHandle);
+close($outputHandle);
+close($notFoundHandle);
