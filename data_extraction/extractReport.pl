90fd919e4280feddf8652b8aee63de0d69e75cd0
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2018-11-10 17:18:48 -0700

    Final clean-up and sort of the data: durations are now given in HH:MM:SS format for easy sorting and comparison; added perl scripts used for clean-up and normalization to the data_extraction directory

--- data_extraction/extractReport.pl
+++ data_extraction/extractReport.pl
@@ -13,7 +13,6 @@ my $duration;
 my $narrative;
 
 foreach my $line (<$fileHandle>) {
-    # <TD BORDERCOLOR=#d0d7e5 ><FONT style=FONT-SIZE:11pt FACE="Calibri" COLOR=#000000>Occurred : 9/18/2018 23:20  (Entered as : 9/18 23:20)<BR>Reported: 9/18/2018 11:09:46 PM 23:09<BR>Posted: 9/27/2018<BR>Location: Layton, UT<BR>Shape: Circle<BR>Duration:45 minutes</FONT></TD>
     if ($line =~ /<TD.*>Occurred\s*:\s*(.*)<BR>Reported\s*:\s*(.*)<BR>Posted\s*:\s*(.*)<BR>Location\s*:\s*(.*)<BR>Shape\s*:\s*(.*)<BR>Duration\s*:\s*(.*)<\/FONT><\/TD>/) {
         $occurred = $1;
         $reported = $2;
@@ -25,12 +24,11 @@ foreach my $line (<$fileHandle>) {
             $occurred = $1;
         }
         next;
-    # <TD BORDERCOLOR=#d0d7e5 ><FONT style=FONT-SIZE:11pt FACE="Calibri" COLOR=#000000>One orb that changed colors, moving in all directions inconsistently.  After about a half hour two more orb like crafts began to draw closer in an inconsistent pace.</FONT></TD>
     } elsif ($line =~ /<TD.*><FONT.*>(.*)<\/FONT><\/TD>/) {
        $narrative = $1; 
        last;
     }
 }
 
-print("\nOccurred|Reported|Posted|Location|Shape|Duration|Narrative\n");
+#print("\nOccurred|Reported|Posted|Location|Shape|Duration|Narrative\n");
 print("$occurred|$reported|$posted|$location|$shape|$duration|$narrative\n");
--- data_extraction/normalizeDuration.pl
+++ data_extraction/normalizeDuration.pl
@@ -1,92 +1,168 @@
 use strict;
 use warnings;
 
+use Text::CSV;
+use Lingua::EN::Words2Nums;
+
+sub max {
+    my $one = shift;
+    my $two = shift;
+
+    if ($one > $two) {
+        return $one;
+    } else {
+        return $two;
+    }
+}
+
 sub normalizeDuration {
     my ($hour, $minute, $second) = @_;
-    if ($second > 60) {
-        my $minutesFromSeconds = $second / 60.0;
+    #print("normalizeDuration args: $hour, $minute, $second\n");
+    if ($second >= 60) {
+        my $minutesFromSeconds = int($second / 60);
         $second -= ($minutesFromSeconds * 60);
         $minute += $minutesFromSeconds;
+        #print("seconds result: $minute, $second\n");
     }
-    if ($minute > 60) {
-        my $hoursFromMinutes = $minute / 60.0;
+    if ($minute >= 60) {
+        my $hoursFromMinutes = int($minute / 60);
         $minute -= ($hoursFromMinutes * 60);
         $hour += $hoursFromMinutes;
+        #print("minutes result: $hour, $minute\n");
     }
-    return sprintf("%02d:%02d:%02d", $hour, $minute, $second);
+    my $retVal = sprintf("%02d:%02d:%02d", $hour, $minute, $second);
+    #print("retVal is: $retVal\n");
+    return $retVal;
 }
 
 sub cleanDuration {
     my $dur = shift;
-    print "cleanDuration args in:  $dur\n";
+    #print "cleanDuration args:  $dur\n";
     my $seconds = 0;
     my $minutes = 0;
     my $hours = 0;
-    if ($dur =~ /[Ss]econds/) {
-        $seconds += 5;
-    } elsif ($dur =~ /[Mm]inutes/) {
-        $minutes += 5;
-    } elsif ($dur =~ /[Hh]ours/) {
-        $hours += 2;
-    } elsif ($dur =~ /(\d+)\s+(\w+)/) {
-        my $value = int($1);
-        my $unit = $2;
-        print("value: $value, unit: $unit\n");
-        if ($unit =~ /sec(?:ond)?s?\.?/i ) {
-            $seconds += $value;
-        } elsif ($unit =~ /min(?:ute)?s?\.?/i) {
-            $minutes += $value; 
-        } elsif ($unit =~ /h(?:ou)?rs?\.?/i) {
-            $hours += $value;
-        }
-    } elsif ($dur =~ /(\d+)-(\d+)\s+(\w+)/) {
+
+    if ($dur =~ /\s*(\d+)\s*\:\s*(\d+)\s*/) {
+        #print ("G\n");
+        $minutes += int($1);
+        $seconds += int($2);
+    } elsif ($dur =~ /\s*(\d+)\s*\:\s*(\d+)\s*\:\s*(\d+)\s*/) {
+        #print ("H\n");
+        $hours += int($1);
+        $minutes += int($2);
+        $seconds += int($3);
+    } elsif ($dur =~ /\s*(\d+)\s*(?:\-|(?:to)|(?:or))\s*(\d+)\s*([A-Za-z]+)/i) {
+        #print ("A\n");
         my $startValue = int($1);
         my $endValue = int($2);
         my $unit = $3;
-        print("start value: $startValue, end value: $endValue, unit: $unit\n");
-        my $value = ($startValue + $endValue) / 2.0;
-        if ($unit =~ /sec(?:ond)?s?\.?/i ) {
+        #print("start value: $startValue, end value: $endValue, unit: $unit\n");
+        my $value;
+        if (($startValue > 0) && ($endValue > 0)) {
+            $value = ($startValue + $endValue) / 2.0;
+        } else {
+            $value = max($startValue, $endValue);
+        }
+        if ($unit =~ /sec?(?:ond)?s?\.?/i ) {
             $seconds += $value;
-        } elsif ($unit =~ /min(?:ute)?s?\.?/i) {
+        } elsif ($unit =~ /mi?n?(?:ute)?s?\.?/i) {
             $minutes += $value; 
-        } elsif ($unit =~ /h(?:ou)?rs?\.?/i) {
+        } elsif ($unit =~ /h(?:ou)?r?s?\.?/i) {
             $hours += $value;
         }
+    } elsif ($dur =~ /(\d+)\s*([A-Za-z]+)\s*(\d+)\s*([A-Za-z]+)/) {
+        #print ("K\n");
+        my $value = int($1);
+        my $unit = $2;
+        my $value2 = int($3);
+        my $unit2 = $4;
+        #print("value: $value, unit: $unit, unit2: $unit2, value2: $value2\n");
+        if ($unit =~ /sec(?:ond)?s?\.?/i ) {
+            $seconds += $value;
+        } elsif ($unit =~ /mi?n?(?:ute)?s?\.?/i) {
+            $minutes += $value; 
+        } elsif ($unit =~ /h(?:ou)?r?s?\.?/i) {
+            $hours += $value;
+        }
+        if ($unit2 =~ /sec(?:ond)?s?\.?/i ) {
+            $seconds += $value2;
+        } elsif ($unit2 =~ /mi?n?(?:ute)?s?\.?/i) {
+            $minutes += $value2; 
+        } elsif ($unit2 =~ /h(?:ou)?r?s?\.?/i) {
+            $hours += $value2;
+        }
+        #print("K out: $hours, $minutes, $seconds\n");
+    } elsif ($dur =~ /(\d+)\s*([A-Za-z]+)/) {
+        #print ("B\n");
+        my $value = int($1);
+        my $unit = $2;
+        #print("value: $value, unit: $unit\n");
+        if ($unit =~ /sec(?:ond)?s?\.?/i ) {
+            $seconds += $value;
+        } elsif ($unit =~ /mi?n?(?:ute)?s?\.?/i) {
+            $minutes += $value; 
+        } elsif ($unit =~ /h(?:ou)?r?s?\.?/i) {
+            $hours += $value;
+        }
+    } elsif ($dur =~ /(.*)sec(?:ond)?s?\.?/i) {
+        #print ("C\n");
+        my $parsed = words2nums($1);
+        if (defined $parsed) {
+            $seconds += $parsed;
+        } else {
+            $seconds += 5;
+        }
+    } elsif ($dur =~ /(.*)min(?:ute)?s?\.?/i) {
+        #print ("D\n");
+        my $parsed = words2nums($1);
+        if (defined $parsed) {
+            $minutes += $parsed;
+        } else {
+            $minutes += 5;
+        }
+    } elsif ($dur =~ /(.*)h(?:ou)?rs?\.?/i) {
+        #print ("E\n");
+        my $parsed = words2nums($1);
+        if (defined $parsed) {
+            $hours += $parsed;
+        } else {
+            $hours += 2;
+        }
+    } elsif ($dur =~ /\s*(\d+)\s*/) {
+        #print ("F\n");
+        $minutes += int($1);
     }
-
     return normalizeDuration($hours, $minutes, $seconds);
 }
 
-# open the output file
-my $outputHandle;
-open($outputHandle, ">", "dataIso.tsv") or die("Cannot open output file!");
 # open the input file
 my $inputHandle;
-open($inputHandle, "<", "dataSmall.tsv") or die("Cannot open input file!");
-foreach my $line (<$inputHandle>) {
-    chomp($line);
-    next if ($line =~ "^Occurred.*");
+open($inputHandle, "<", "data.tsv") or die("Cannot open input file!");
+my $tsv = Text::CSV->new({binary => 1, sep_char => "\t", eol => "\r\n"}) or die("Error creating TSV reader!");
+my @rows;
+$tsv->column_names($tsv->getline($inputHandle));
+while(my $row = $tsv->getline($inputHandle)) {
     # Occurred,Reported,Location,Latitude,Longitude,Shape,Duration,Narrative
-    $line =~ /(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)\t(.*)\s+/;
-    my $occurred = $1;
-    my $reported = $2;
-    my $location = $3;
-    my $latitude = $4;
-    my $longitude = $5;
-    my $shape = $6;
-    my $duration = $7;
-    my $narrative = $8;
-    if (!defined $shape || ($shape eq '')) {
-        $shape = "Other";
+    my $shape = $row->[5];
+    my $duration = $row->[6];
+
+    if (!defined $shape || ($shape eq '') || $shape =~ /unknown/i) {
+        $row->[5] = "Other";
     }
-    if ($shape =~ /[Uu]nknown/) {
-        $shape = "Other";
-    }
+
+    #print("duration: $duration\n");
     if (defined $duration && $duration ne '') {
         $duration = cleanDuration($duration);
+        next if ($duration eq "00:00:00");
+        $row->[6] = $duration;
     }
-    print $outputHandle "$occurred\t$reported\t$location\t$latitude\t$longitude\t$shape\t$duration\t$narrative\n";
-}
 
+    push @rows, $row;
+}
 close($inputHandle);
+
+# open the output file
+my $outputHandle;
+open($outputHandle, ">", "dataNorm.tsv") or die("Cannot open output file!");
+$tsv->print($outputHandle, $_) for @rows;
 close($outputHandle);
