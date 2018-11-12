b471ab5a1009b2faaaa34ae0e141b1ed9c63022c
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2018-11-12 11:41:33 -0700

    Add class stubs for all of the major elements of the design

---
+++ js/DateSelector.js
@@ -0,0 +1,24 @@
+/*
+    CS-5890 Project:  UFO Report Explorer
+    The Best Team
+	Team Members:	 Jonathon Pearson	       Richard Scott McNew
+	Email Addresses: jnewellpearson@gmail.com  richard.scott.mcnew@gmail.com
+	A Numbers:	     A01108018	               A02077329
+
+    Copyright (C) 2018, Jonathon Pearson.
+    Copyright (C) 2018, Richard Scott McNew.  
+*/
+/* Turn-on strict mode for easier debugging */
+'use strict';
+
+class DateSelector {
+
+    constructor() {
+
+    }
+
+    update() {
+
+    }
+
+}
