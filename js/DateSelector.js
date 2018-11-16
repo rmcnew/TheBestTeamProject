95f8912625dbea916858244534e0de4bde8e9975
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2018-11-15 19:49:19 -0700

    Date selector half way

--- js/DateSelector.js
+++ js/DateSelector.js
@@ -16,6 +16,7 @@ class DateSelector {
     constructor() {
         this.minDate = d3.min(window.ufoReports, d => d.Occurred);
         this.maxDate = d3.max(window.ufoReports, d => d.Occurred);
+        let widthScaleFactor = 0.8;
         console.log("min date: " + this.minDate);
         console.log("max date: " + this.maxDate);
 
@@ -29,38 +30,43 @@ class DateSelector {
 			.attr("min", this.minDate)
 			.attr("max", this.maxDate);
 
-        let dateSelector = d3.select("#date-selector");
-        dateSelector.append("br");
-		dateSelector.append("input")
+        let dateSelectorDiv = d3.select("#date-selector");
+		// create a scale for the date range
+        let rangeWidth = dateSelectorDiv.node().getBoundingClientRect().width * widthScaleFactor;
+        this.dateScale = d3.scaleTime()
+            .domain([new Date(this.minDate), new Date(this.maxDate)])
+            .range([0, rangeWidth]);
+        dateSelectorDiv.append("br");
+		dateSelectorDiv.append("input")
 			.attr("id", "date-selector-lower")
 			.attr("type", "range")
+            .attr("min", "0")
+            .attr("max", rangeWidth)
 			.attr("multiple", "")
 			.attr("class", "multirange original")
-			.attr("value", "10")
-			.on("input", function() {
-				d3.select("#date-selector-upper")
-					.style("--low", d3.select("#date-selector-lower").property("value"));
-			});
-		dateSelector.append("input")
+            .style("width", rangeWidth)
+			.property("value", this.dateScale(new Date(this.minDate)))
+            .on("input", function() {
+                d3.select("#startDate")
+                    .property("value", window.dateSelector.dateScale.invert(d3.select("#date-selector-lower").property("value")).toISOString());
+            });
+		dateSelectorDiv.append("input")
 			.attr("id", "date-selector-upper")
 			.attr("type", "range")
+            .attr("min", "0")
+            .attr("max", rangeWidth)
 			.attr("multiple", "")
 			.attr("class", "multirange ghost")
-			.style("--low", "11%")
-			.style("--high","79%")
-			.attr("value", "80")
-			.on("input", function() {
-				d3.select("#date-selector-upper")
-					.style("--high", d3.select("#date-selector-upper").property("value"));
-			});
-		// create a scale for the date range
-        let svg = dateSelector.append("svg")
+            .style("width", rangeWidth)
+            .property("value", this.dateScale(new Date(this.maxDate)))
+            .on("input", function() {
+                d3.select("#stopDate")
+                    .property("value", window.dateSelector.dateScale.invert(d3.select("#date-selector-upper").property("value")).toISOString());
+            });
+        let svg = dateSelectorDiv.append("svg")
             .attr("height", "20")
-            .attr("width", dateSelector.node().getBoundingClientRect().width);
-        let dateScale = d3.scaleTime()
-            .domain([this.minDate, this.maxDate])
-            .range([0, dateSelector.node().getBoundingClientRect().width]);
-        let dateAxis = d3.axisBottom(dateScale);
+            .attr("width", rangeWidth);
+        let dateAxis = d3.axisBottom(this.dateScale);
         svg.append("g").call(dateAxis);
 
     }
