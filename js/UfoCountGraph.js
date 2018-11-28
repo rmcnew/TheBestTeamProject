f64f9e684bdb6e3879701090f9e11f112e1a474f
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2018-11-28 00:01:25 -0700

    Checkpoint: starting on Ufo Count Graph

--- js/UfoCountGraph.js
+++ js/UfoCountGraph.js
@@ -14,19 +14,44 @@
 class UfoCountGraph {
 
     constructor(width, height) {
-/*
-        let countGraphSvg = d3.select("#line-chart")
+		// create the target SVG
+        this.countGraphSvg = d3.select("#line-chart")
             .append("svg")
             .attr("width", width)
             .attr("height", height)
             .attr("id", "ufoCountGraphSvg");
-        let drawCountGraph(data) {
 
-        }
-        window.ufoDatabase.runQueryWithCallBack(
-            "SELECT COUNT(*) FROM UFO_REPORTS WHERE ", 
-            drawCountGraph)
+		// query to get the needed data
+		this.sqlQuery = "select count(ID) as SIGHTING_COUNT, strftime('%Y-%m', OCCURRED) as SIGHTING_DATE  from UFO_REPORTS group by strftime('%Y-%m', OCCURRED);";
+		
+		// callback to draw the graph using the query result data
+		this.drawCountGraph = function(data) {
+			// get the svg and its dimensions
+			let svg = d3.select("#ufoCountGraphSvg");
+			let svgWidth = svg.node().getBoundingClientRect().width;
+			let svgHeight = svg.node().getBoundingClientRect().height;
+			// build the time scale
+			let minDate = d3.min(data, d => d.SIGHTING_DATE);
+			let maxDate = d3.max(data, d => d.SIGHTING_DATE);
+			console.log("UfoCountGraph:  minDate: " + minDate + ", maxDate: " + maxDate);
+			let timeScale = d3.scaleTime()
+				.domain([minDate, maxDate])
+				.range([0, svgWidth]);
+			// build the count scale			
+			let minCount = 0;
+			let maxCount = d3.max(data, d => d.SIGHTING_COUNT);
+			console.log("UfoCountGraph:  minCount: " + minCount + ", maxCount: " + maxCount);
+			let countScale = d3.scaleLinear()
+				.domain([minCount, maxCount])
+				.range([0, svgHeight]);
+			
+/*
+			let line = d3.line()
+				.x( d => 	
 */
+		};
+
+        window.ufoDatabase.runQueryWithCallBack(this.sqlQuery, this.drawCountGraph);
     }
 
     update() {
